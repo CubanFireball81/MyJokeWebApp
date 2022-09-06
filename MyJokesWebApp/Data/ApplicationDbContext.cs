b71a7bdd52f0e6f50a4f5f6ad2f7f314e9f14bf2
Alejandro Santiago <h4k3r322@Alejandros-MBP.hitronhub.home>
2022-09-05 17:16:28 -0800

    Adds JokesController along with all the Views for the Jokes Page.

--- MyJokesWebApp/Data/ApplicationDbContext.cs
+++ MyJokesWebApp/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MyJokesWebApp.Models;
 
 namespace MyJokesWebApp.Data;
 
@@ -9,5 +10,6 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+    public DbSet<MyJokesWebApp.Models.Joke>? Joke { get; set; }
 }
 
