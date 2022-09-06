76a3e13e257cfabeb38bc6b37ec029c7b44f6553
Alejandro Santiago <h4k3r322@Alejandros-MBP.hitronhub.home>
2022-09-05 16:55:14 -0800

    Adds Joke Model, Properties, and Constructor.

---
+++ MyJokesWebApp/Models/Joke.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace MyJokesWebApp.Models
+{
+    public class Joke
+    {
+        //  Properties
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        //  Constructor
+        public Joke()
+        {
+
+        }
+    }
+}
+
